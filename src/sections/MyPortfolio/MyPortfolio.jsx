30dfe767ae68604e89240925035dc4230283a2ed
krishsonvane14 <155946430+krishsonvane14@users.noreply.github.com>
2024-09-25 06:23:53 -0700

    removing index.json file

--- src/sections/MyPortfolio/MyPortfolio.jsx
+++ src/sections/MyPortfolio/MyPortfolio.jsx
@@ -1,7 +1,33 @@
 import styles from './MyPortfolioStyles.module.css';
-import data from '../../data/index.json';
 
 export default function MyPortfolio() {
+  const portfolioData = [
+    {
+      id: "1",
+      src: "/src/assets/gymbro.svg",
+      title: "GymBro",
+      description:
+        "GymBro is a personal project designed to enhance fitness tracking by generating personalized workout plans based on user preferences.",
+      link: "https://github.com/krishsonvane14/gymbro",
+    },
+    {
+      id: "2",
+      src: "/src/assets/piggypro.svg",
+      title: "PiggyPro",
+      description:
+        "PiggyPro allows users to input their income, expenses, and budgets, and receive personalized financial advice based on their financial data.",
+      link: "https://github.com/krishsonvane14/piggypro",
+    },
+    {
+      id: "3",
+      src: "/src/assets/portfolioWebsite.svg",
+      title: "This Website!",
+      description:
+        "A responsive React Portfolio Website with a light & dark mode showcasing my projects and links to my socials!",
+      link: "https://github.com/krishsonvane14/portfolio-website",
+    },
+  ];
+
   return (
     <section className={styles.portfolioSection} id="MyPortfolio">
       <div className={styles.portfolioContainerBox}>
@@ -11,7 +37,7 @@ export default function MyPortfolio() {
         </div>
       </div>
       <div className={styles.portfolioSectionContainer}>
-        {data?.portfolio?.map((item, index) => (
+        {portfolioData.map((item, index) => (
           <div key={index} className={styles.portfolioSectionCard}>
             <div className={styles.portfolioSectionImg}>
               <img src={item.src} alt={item.title} />
@@ -19,7 +45,7 @@ export default function MyPortfolio() {
             <div className={styles.portfolioSectionCardContent}>
               <h3 className={styles.portfolioSectionTitle}>{item.title}</h3>
               <p className={styles.projectDescription}>{item.description}</p>
-              <a href='https://github.com/krishsonvane14' target="_blank" className={styles.portfolioLink}>
+              <a href={item.link} target="_blank" rel="noopener noreferrer" className={styles.portfolioLink}>
                 View on GitHub
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
